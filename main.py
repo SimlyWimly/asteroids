e281f03812e7987e383beb2c11fa75b9cb43db64
SimlyWimly <simlywimly@gmail.com>
2024-09-08 19:35:32 -0500

    C2 Player L8 Groups

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    toggle = True
-    while toggle == True:
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0,0,0))
+        
+        for item in drawable:
+            item.draw(screen)
+        for item in updatable:
+            item.update(dt)
+
         pygame.display.flip()
-    
+        dt = clock.tick(60)/1000    
 
 if __name__ == "__main__":
     main()
